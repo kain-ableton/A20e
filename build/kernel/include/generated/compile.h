4a9fd990f6c9486176075836eef3427a255814c9
kain-ableton <kdsama53@gmail.com>
2021-01-19 03:50:21 +0000

    NetHunter Kernel

---
+++ build/kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Tue Jan 19 03:49:15 GMT 2021"
+#define LINUX_COMPILE_BY "kd"
+#define LINUX_COMPILE_HOST "spectre"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
